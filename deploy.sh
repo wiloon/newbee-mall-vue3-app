088f7df7c7560674b83f7fc0470edf13ad70478b
Yue <wiloon.wy@gmail.com>
2023-05-06 17:40:41 +0800

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ npm run build
 package_name="newbee-mall-app.tar.gz"
 gtar zcvf ${package_name} -C dist .
 scp ${package_name} aliyun:/tmp
+cp ${package_name} /Users/wiloon/tmp
 ansible -i 'wiloon.com,' all  -m shell -a 'tar zxvf /tmp/newbee-mall-app.tar.gz -C /root/volumes/nginx-www/_data/newbee-mall-app/' -u root
 
