e0a26cf9631e66e630439979e49764be0ff30e3e
Yue <wiloon.wy@gmail.com>
2023-05-08 09:20:01 +0800

    show qrcode

--- src/views/User.vue
+++ src/views/User.vue
@@ -41,8 +41,8 @@
         </li>
     </ul>
       <div style="margin-left: 30px">
-          <div>店主名：{{state.shopOwner}}</div>
-          <div>店铺名：{{state.shopName}}</div>
+          <div>店铺：{{state.shopName}}</div>
+          <div>店主：{{state.shopOwner}}</div>
           <div>店铺地址：{{state.shopAddress}}</div>
           <br/>
           <div>店铺二维码：<QrcodeVue :value="state.shopAddress" size="75" level="H" /></div>
